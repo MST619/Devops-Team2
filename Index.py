885ec3f824490519f3046dd728c5b20c6b1e487d
DicksonKuan <s10192803@connect.np.edu.sg>
2021-10-26 18:47:44 +0800

    Program layout
    To add functions that are needed for the game

--- Index.py
+++ Index.py
@@ -1,1 +1,21 @@
-print("hello world")
+'''
+S10192803, Dickson Kuan,
+
+
+'''
+#Lists and Variable 
+
+
+'''
+Functions =========================================================================================================================================================
+
+'''
+
+def menu():
+	print("This is for menu Functions")
+
+def InitLoad():
+	print("To check for maze file")
+
+def printLayout():
+	print("To print the game layout")
