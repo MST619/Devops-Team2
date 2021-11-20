3ef4c80d5c0deeb413473db4c862981d614a37c0
JeremiahLDX00 <jeremiahlong777@gmail.com>
2021-11-20 16:34:04 +0800

    Done the board and the setup for the game, building randomizers, map and user input (No validation yet)

--- Index.py
+++ Index.py
@@ -28,6 +28,40 @@ def menu():
 #Feature 3
 def newGame(): 
 	print("To create new game map")
+	
+	GameMap = [['','','',''],\
+				['','','',''],\
+				['','','',''],\
+				['','','','']]
+	# building randomizer
+	RandBuilding1 = random.randint(0,4)
+	RandBuilding2 = random.randint(0,4)
+
+	# buildings of 8 copies each
+	buildingcopies = [['HSE',8],['FAC',8],['SHP',8],['HWY',8],['BCH',8]]
+	# building code for referencing
+	buildingcode = ['HSE','FAC','SHP','HWY','BCH']
+
+	turn = 1
+	print("Turn {}".format(turn));
+
+	# Board creation (X and Y coordinates, placing the +, - and |)
+	print("{:>5}{:>6}{:>6}{:>6}".format("A", "B", "C", "D"))
+	for row in range(len(GameMap)):
+		column = len(GameMap[row]) #defining the columns
+		print(' ' + '+-----'*column+'+')
+		print(row+1, end = '')
+		for line in range(len(GameMap[row])): #defining the rows
+			print('|{:^5}'.format(GameMap[row][line]), end = '')
+		print('|')
+	print(' ' + '+-----'*column + '+')
+
+	# Menu options
+	print('1. Build a {}'.format(buildingcode[RandBuilding1]), '\n', '2. Build a {}'.format(buildingcode[RandBuilding2]), '\n', '3. See remaining buildings\n','4. See current score\n\n','5. Save game\n','0. Exit to main menu')
+	userChoice = input('Your choice?')
+
+	#returning choice, map, building randomizer, turns and number of copies
+	return userChoice, GameMap, RandBuilding1, RandBuilding2, turn, buildingcopies
 
 #Feature 4
 def saveGame():
