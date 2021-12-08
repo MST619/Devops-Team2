dee8b63f5e840c2a5162d768f0867a5215ca0716
Dickson Kuan <s10192803@connect.np.edu.sg>
2021-12-08 10:25:31 +0800

    Feature 7.1 tested
    Added test scripts and clean up code file for feature 7.1

--- src/Game.py
+++ src/Game.py
@@ -39,74 +39,89 @@ def newGame():
 	# returning choice, map, building randomizer, turns and number of copies
 	return userInput, GameMap, RandBuilding1, RandBuilding2, turn, buildings
 
-# Feature 7.1 
+# Feature 7.2 
+# Find user input location to place the building
+def inputLocationValidation(userInput):
+	validation = ["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"]
+	if userInput in validation:
+		return True
+	else:
+		return False
+
+def checkLocList(userInput):
+	locList = [['A',0],['B',1],['C',2],['D',3]]
+	for row in locList:
+		if userInput in row:
+			return row[1]
+	return None
+
+def checkValidInput(GameMap,location):
+	if (0 <= location[0] + 1 < len(GameMap[0])):
+		if (GameMap[location[0] + 1][location[1]] != ""):
+			return True
+
+	if (0 <= location[0] - 1 < len(GameMap[0])):
+		if (GameMap[location[0] - 1][location[1]] != ""):
+			return True
+
+	if (0 <= location[1] + 1 < len(GameMap)):
+		if (GameMap[location[0]][location[1] + 1] != ""):
+			return True
+
+	if (0 <= location[1] - 1 < len(GameMap)):
+		if (GameMap[location[0]][location[1] - 1] != ""):
+			return True
+
+	return False
+
+def checkBuildingLeft(newBuilding):
+	# to check if there is any building left
+	while True:
+		RandBuilding = random.randint(0,4)
+		if newBuilding[RandBuilding][1] <= 0:
+			continue
+        
+		return RandBuilding
+		# if newBuilding[RandBuilding2][1] <= 0:
+		# 	RandBuilding2 = random.randint(0,4)
+		# 	continue
+        
+		
+
+# Feature 7.1
 def printLayout(GameMap, buildings, turn, newBuilding):
 	# print("To print the game layout")
 	check = True
 	while check:
-		validation = ["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"]
 		userInput = input('Build where? \n')
 		userInput = userInput.upper()
-		if userInput in validation:
-	# Feature 7.2 
-	# Find user input location to place the building
-			locList = [['A',0],['B',1],['C',2],['D',3]]
-			for row in locList:
-
-				if userInput[0] in row:
-					location = [int(userInput[1])-1,row[1]]
-
-					if (GameMap[location[0]] [location[1]] == ''):
-						validInput = False
-
-						if (0 <= location[0] + 1 < len(GameMap[0])):
-							if (GameMap[location[0] + 1][location[1]] != ""):
-								validInput = True
-
-						if (0 <= location[0] - 1 < len(GameMap[0])):
-							if (GameMap[location[0] - 1][location[1]] != ""):
-								validInput = True
-
-						if (0 <= location[1] + 1 < len(GameMap)):
-							if (GameMap[location[0]][location[1] + 1] != ""):
-								validInput = True
-
-						if (0 <= location[1] - 1 < len(GameMap)):
-							if (GameMap[location[0]][location[1] - 1] != ""):
-								validInput = True
-
-						if validInput == True or turn == 1:
-							GameMap[location[0]][location[1]] = newBuilding[buildings][0]
-							check = False
-						else:
-							print('You must place near an existing building')
+		if inputLocationValidation(userInput):
+			checkLocListResult = checkLocList(userInput[0])
+			if checkLocListResult != None:
+				location = [int(userInput[1])-1,checkLocListResult]
+				print(location)
+				if (GameMap[location[0]] [location[1]] == ''):
+					if checkValidInput(GameMap, location) == True or turn == 1:
+						GameMap[location[0]][location[1]] = newBuilding[buildings][0]
+						check = False
 					else:
-						print('This location already has a building, please choose another location.')
+						print('You must place near an existing building')
+				else:
+					print('This location already has a building, please choose another location.')
+					continue
+					
 		else:
 			print("Please provide a valid input")
-			continue
 
 	# display turns
 	turn += 1
 
 	# building randomizer
-	RandBuilding1 = random.randint(0,4)
-	RandBuilding2 = random.randint(0,4)
-
 	buildingCode = ['HSE','FAC','SHP','HWY','BCH']
 
 	newBuilding[buildings][1] -= 1
-
-	while True:
-		if newBuilding[RandBuilding1][1] <= 0:
-			RandBuilding1 = random.randint(0,4)
-			continue
-        
-		if newBuilding[RandBuilding2][1] <= 0:
-			RandBuilding2 = random.randint(0,4)
-			continue
-        
-		break
+	RandBuilding1 = checkBuildingLeft(newBuilding)
+	RandBuilding2 = checkBuildingLeft(newBuilding)
 
 	if turn <= 16:
 		print('Turn {}'.format(turn))
