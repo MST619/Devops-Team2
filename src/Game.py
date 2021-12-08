0887f6d2410e8a4d1866a9d2231a5a9a9450a4b4
Dickson Kuan <s10192803@connect.np.edu.sg>
2021-12-08 08:51:52 +0800

    Update new game feature into main branch

--- src/Game.py
+++ src/Game.py
@@ -3,6 +3,42 @@ import random
 7. Game Function =========================================================================================================================================================
 '''
 
+def newGame(): 
+	# print("To create new game map")
+	GameMap = [['','','',''],\
+				['','','',''],\
+				['','','',''],\
+				['','','','']]
+	# building randomizer
+	RandBuilding1 = random.randint(0,4)
+	RandBuilding2 = random.randint(0,4)
+
+	# buildings of 8 copies each
+	buildings = [['HSE',8],['FAC',8],['SHP',8],['HWY',8],['BCH',8]]
+	# building code for referencing
+	buildingCode = ['HSE','FAC','SHP','HWY','BCH']
+
+	turn = 1
+	print("Turn {}".format(turn))
+
+	# Board creation (X and Y coordinates, placing the +, - and |)
+	print("{:>5}{:>6}{:>6}{:>6}".format("A", "B", "C", "D"))
+	for row in range(len(GameMap)):
+		column = len(GameMap[row]) #defining the columns
+		print(' ' + '+-----'*column+'+')
+		print(row+1, end = '')
+		for line in range(len(GameMap[row])): #defining the rows
+			print('|{:^5}'.format(GameMap[row][line]), end = '')
+		print('|')
+	print(' ' + '+-----'*column + '+')
+
+	# Menu options
+	print(' 1. Build a {}'.format(buildingCode[RandBuilding1]), '\n', '2. Build a {}'.format(buildingCode[RandBuilding2]), '\n', '3. See remaining buildings\n','4. See current score\n\n','5. Save game\n','0. Exit to main menu')
+	userInput = input('Your choice?\n')
+
+	# returning choice, map, building randomizer, turns and number of copies
+	return userInput, GameMap, RandBuilding1, RandBuilding2, turn, buildings
+
 # Feature 7.1 
 def printLayout(GameMap, buildings, turn, newBuilding):
 	# print("To print the game layout")
